648b3e7e5301a018d692675ef646e3d136682789
Nguyen Trong Khai <tkhai12386@gmail.com>
2025-05-17 09:13:17 +0700

    update dashboard

--- resources/views/admin-template/template-basic/layouts/master.blade.php
+++ resources/views/admin-template/template-basic/layouts/master.blade.php
@@ -17,7 +17,22 @@
     @include('layouts.partials.header')
 </header>
 <main>
-    @yield('content')
+    <div class="container-scroller">
+        @include(admin_template_basic_theme('partials.sidebar'))
+        <div class="container-fluid page-body-wrapper">
+            @include(admin_template_basic_theme('partials.navbar'))
+            <div class="main-panel">
+                @yield('content')
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a
+                                href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
 </main>
 <footer>
     @include(admin_template_basic_theme('layouts.footer-admin'))
