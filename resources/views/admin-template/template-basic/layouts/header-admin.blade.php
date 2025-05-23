b74c64f06ef039c0060bc4855d5809c32749cb25
Nguyen Trong Khai <tkhai12386@gmail.com>
2025-05-23 20:49:24 +0700

    update dashboard

--- resources/views/admin-template/template-basic/layouts/header-admin.blade.php
+++ resources/views/admin-template/template-basic/layouts/header-admin.blade.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css')}}">
 <!-- endinject -->
 <!-- Plugin css for this page -->
+<link rel="stylesheet" href="{{asset('assets/libs/bootstrap/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{ asset('assets/vendors/jvectormap/jquery-jvectormap.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/vendors/owl-carousel-2/owl.carousel.min.css') }}">
@@ -16,3 +17,4 @@
 <script src="{{ asset('assets/libs/jquery/jquery-3.7.1.min.js') }}"></script>
 <script src="{{ asset('assets/libs/dataTables/js/dataTables.js') }}"></script>
 <script src="{{ asset('assets/libs/dataTables/js/dataTables.bootstrap5.js') }}"></script>
+<script src="{{ asset('assets/libs/bootstrap/bootstrap.bundle.min.js') }}"></script>
--- resources/views/admin-template/template-basic/partials/navbar.blade.php
+++ resources/views/admin-template/template-basic/partials/navbar.blade.php
@@ -15,6 +15,7 @@
                 </form>
             </li>
         </ul>
+
         <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item dropdown d-none d-lg-block">
                 <a class="nav-link btn btn-success create-new-button" id="createbuttonDropdown"
@@ -159,6 +160,24 @@
                     <p class="p-3 mb-0 text-center">Xem tất cả thông báo</p>
                 </div>
             </li>
+            <li class="nav-item dropdown border-left">
+                <button id="darkModeToggle" class="dark-mode-toggle" aria-label="Toggle dark mode">
+                    <svg class="sun-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <circle cx="12" cy="12" r="5"></circle>
+                        <line x1="12" y1="1" x2="12" y2="3"></line>
+                        <line x1="12" y1="21" x2="12" y2="23"></line>
+                        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                        <line x1="1" y1="12" x2="3" y2="12"></line>
+                        <line x1="21" y1="12" x2="23" y2="12"></line>
+                        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                    </svg>
+                    <svg class="moon-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                    </svg>
+                </button>
+            </li>
             <li class="nav-item dropdown">
                 <a class="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
                     <div class="navbar-profile">
@@ -197,6 +216,7 @@
                 </div>
             </li>
         </ul>
+
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
                 data-toggle="offcanvas">
             <span class="mdi mdi-format-line-spacing"></span>
