5861042c5d1cd35eb500ed313ea3117b3f96d8e1
Nguyen Trong Khai <tkhai12386@gmail.com>
2025-06-12 00:39:31 +0700

    update dashboard

--- resources/views/admin-template/template-basic/assets/js/script.js
+++ resources/views/admin-template/template-basic/assets/js/script.js
@@ -1,3 +1,26 @@
+const html = $('html');
+
+const savedTheme = localStorage.getItem('theme');
+const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
+    html.removeClass('light');
+    html.addClass('dark');
+}
+
+// Toggle dark mode
+$('#darkModeToggle').on('click', function () {
+    if (html.hasClass('dark')) {
+        html.removeClass('dark');
+        html.addClass('light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        html.removeClass('light');
+        html.addClass('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
 $(document).ready(function () {
     $('button[type="reset"][data-bb-toggle="btn-with-href"]').on('click', function (e) {
         e.preventDefault();
@@ -5,28 +28,3 @@ $(document).ready(function () {
         window.location.href = $(this).data('url');
     })
 })
-
-$(document).ready(function () {
-    const html = $('html');
-
-    const savedTheme = localStorage.getItem('theme');
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-    if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
-        html.removeClass('light');
-        html.addClass('dark');
-    }
-
-    // Toggle dark mode
-    $('#darkModeToggle').on('click', function () {
-        if (html.hasClass('dark')) {
-            html.removeClass('dark');
-            html.addClass('light');
-            localStorage.setItem('theme', 'light');
-        } else {
-            html.removeClass('light');
-            html.addClass('dark');
-            localStorage.setItem('theme', 'dark');
-        }
-    });
-});
